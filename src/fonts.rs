6e5de6f3ca9d6fee481d2cf23222291bc6570700
York Xiang <bombless@126.com>
2023-04-02 18:32:04 +0800

    Fix for Linux systems

--- src/fonts.rs
+++ src/fonts.rs
@@ -1,5 +1,5 @@
-use std::path::{Path, PathBuf};
 use std::fs::read;
+use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 /// Searches for fonts.
@@ -11,6 +11,39 @@ impl FontSearcher {
     /// Create a new, empty system searcher.
     pub fn new() -> Self {
         Self { fonts: vec![] }
+    }
+
+    #[cfg(not(any(
+        target_os = "macos",
+        target_os = "windows",
+        target_os = "linux",
+        target_os = "freebsd",
+        target_os = "netbsd",
+        target_os = "openbsd"
+    )))]
+    pub fn search_system(&mut self) {}
+
+    /// Search for fonts in netbsd/openbsd etc.
+    #[cfg(any(target_os = "netbsd", target_os = "openbsd"))]
+    pub fn search_system(&mut self) {
+        self.search_dir("/usr/X11R7/lib/X11/fonts");
+        self.search_dir("/usr/X11R7/lib/X11/fonts/OTF");
+        self.search_dir("/usr/X11R7/lib/X11/fonts/TTF");
+        self.search_dir("/usr/X11R7/lib/X11/fonts/Type1");
+
+        if let Some(dir) = dirs::font_dir() {
+            self.search_dir(dir);
+        }
+    }
+
+    /// Search for fonts in fedora/ubuntu/freebsd etc.
+    #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+    pub fn search_system(&mut self) {
+        self.search_dir("/usr/share/fonts");
+
+        if let Some(dir) = dirs::font_dir() {
+            self.search_dir(dir);
+        }
     }
 
     /// Search for fonts in the macOS system font directories.
@@ -26,10 +59,9 @@ impl FontSearcher {
     }
 
     /// Search for fonts in the Windows system font directories.
-    #[cfg(windows)]
+    #[cfg(target_os = "windows")]
     pub fn search_system(&mut self) {
-        let windir =
-            std::env::var("WINDIR").unwrap_or_else(|_| "C:\\Windows".to_string());
+        let windir = std::env::var("WINDIR").unwrap_or_else(|_| "C:\\Windows".to_string());
 
         self.search_dir(Path::new(&windir).join("Fonts"));
 
@@ -60,6 +92,17 @@ impl FontSearcher {
         }
     }
 
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
+    fn contains_cjk(s: &str) -> bool {
+        for c in s.chars() {
+            if c >= '\u{4e00}' && c <= '\u{9fff}' {
+                return true;
+            }
+        }
+        false
+    }
+
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
     /// Index the fonts in the file at the given path.
     pub fn search_file(&mut self, path: impl AsRef<Path>) {
         let content = read(path.as_ref()).unwrap();
@@ -67,13 +110,21 @@ impl FontSearcher {
         for index in 0..count {
             if let Ok(x) = ttf_parser::Face::parse(&content, index) {
                 for x in x.names() {
-                    if x.to_string().map(|x| x.starts_with("Sti")) != Some(true) {
+                    if x.name_id != 4 {
                         continue;
                     }
-                    println!("{}", x.to_string().unwrap_or_else(|| format!("{:?}", x)))
+                    if x.to_string().map(|x| Self::contains_cjk(&x)) == Some(true) {
+                        self.fonts.push((path.as_ref().to_path_buf(), content));
+                        return;
+                    }
                 }
-            }        
+            }
         }
+    }
+    #[cfg(not(any(target_os = "windows", target_os = "linux")))]
+    /// Index the fonts in the file at the given path.
+    pub fn search_file(&mut self, path: impl AsRef<Path>) {
+        let content = read(path.as_ref()).unwrap();
         self.fonts.push((path.as_ref().to_path_buf(), content));
     }
 }
