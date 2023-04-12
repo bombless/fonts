610c6d8cf4cab059a49fb6fdcb695f25d3e40005
York Xiang <bombless@126.com>
2023-04-12 13:29:54 +0800

    Try new things

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod gpu_cache;
-mod fonts;
+mod font_searcher;
+mod fc;
+mod font;
+mod ft;
 
 use std::path::Path;
 use std::fs::read;
 
 fn main() {
-    let mut font_searcher = fonts::FontSearcher::new();
+    let mut font_searcher = font_searcher::FontSearcher::new();
     font_searcher.search_system();
     // for f in &font_searcher.fonts {
     //     println!("{:?}", f.0)
